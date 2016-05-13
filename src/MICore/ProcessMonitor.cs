386ab74de3671307602b238778e1794102f5cb30
Ed Munoz <edmunoz@microsoft.com>
2016-05-12 17:37:27 -0700

    Monitor the shell pid and remove the exit fifo
    The exit fifo was causing issues in the case that MIEngine had already exited
    but the shell started executing the EXIT trap. In that case, it would block on
    echoing 'exit' to a fifo since there were no readers.

---
+++ src/MICore/ProcessMonitor.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+
+namespace MICore
+{
+    public class ProcessMonitor: IDisposable
+    {
+        private int _processId;
+        private Timer _monitorTimer;
+
+        public ProcessMonitor(int processId)
+        {
+            if (!PlatformUtilities.IsLinux() && !PlatformUtilities.IsOSX())
+            {
+                throw new NotImplementedException();
+            }
+
+            _processId = processId;
+        }
+
+        public void Start()
+        {
+            _monitorTimer = new Timer(MonitorForExit, null, TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(100));
+        }
+
+        public event EventHandler ProcessExited;
+
+        private bool HasExited()
+        {
+            // TODO change to getpgid
+            return UnixNativeMethods.Kill(_processId, 0) != 0;
+        }
+
+        private void MonitorForExit(object o)
+        {
+            if (HasExited() && ProcessExited != null)
+            {
+                _monitorTimer.Change(Timeout.Infinite, Timeout.Infinite);
+                ProcessExited(this, null);
+            }
+        }
+
+        public void Dispose()
+        {
+            _monitorTimer.Dispose();
+        }
+    }
+}
